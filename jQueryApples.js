cb319a780e3610807052109e891923622661adcc
Wendy <wendygoodwin@mac.com>
2015-09-10 22:52:26 -0700

    jQuery working

--- jQueryApples.js
+++ jQueryApples.js
@@ -2,9 +2,45 @@
 
 		$('a').click(function(event) {
 			alert('apple pies');
-			
 		});
 
+		var red = document.createElement('div');
+		var redText = document.createTextNode('red');
+		red.setAttribute('id', 'red');
+		red.appendChild(redText);
+
+		
+		$('#apple').append(red);
+		$('#red').last().addClass('red');
 		
 
+		var green = document.createElement('div');
+		var greenText= document.createTextNode('green');
+		green.appendChild(greenText);
+		green.setAttribute('id', 'green');
+		green.appendChild(greenText);
+
+		$('#apple').append(green);
+		$('#green').last().addClass('green');
+		
+		var poisined = document.createElement('div');
+		var poisinedText = document.createTextNode('poisined');
+		poisined.appendChild(poisinedText);
+		poisined.setAttribute('id', 'poisined');
+		poisined.appendChild(poisinedText);
+
+		$('#apple').append(poisined);
+		$('#poisined').last().addClass('poisined');
+
+		$('#poisined').click(function() {
+			$('#red:first').fadeToggle('slow');
+		});
+		$('#poisined').click(function() {
+			console.log('banana');
+			$('#green:last').fadeToggle('slow');
+		});
+
 	});
+
+
+
